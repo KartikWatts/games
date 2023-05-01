b34d9ae2cda3264283fbc382f6be08cca7dbe3df
KartikWatts <arorakartik4@gmail.com>
2023-05-01 18:10:17 +0530

    implementing mob death functionality and animation

--- platformer-game/mobs/frog.gd
+++ platformer-game/mobs/frog.gd
@@ -5,10 +5,14 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var player
 var chase = false
 
+func _ready():
+	get_node("AnimatedSprite2D").play("Idle")
+
 func _physics_process(delta):
 	velocity.y += gravity * delta
 	if chase == true:
-		get_node("AnimatedSprite2D").play("Jump")		
+		if get_node("AnimatedSprite2D").animation != "Death":
+			get_node("AnimatedSprite2D").play("Jump")		
 		player = get_node("../../Player/Player")
 		var direction = (player.position - self.position).normalized()
 		if direction.x > 0:
@@ -17,8 +21,9 @@ func _physics_process(delta):
 			get_node("AnimatedSprite2D").flip_h = false			
 		velocity.x = direction.x * SPEED
 	else:
+		if get_node("AnimatedSprite2D").animation != "Death":
+			get_node("AnimatedSprite2D").play("Idle")	
 		velocity.x = 0
-		get_node("AnimatedSprite2D").play("Idle")		
 		
 	move_and_slide()
 
@@ -30,3 +35,10 @@ func _on_player_detection_body_entered(body):
 func _on_player_detection_body_exited(body):
 	if body.name == "Player":
 		chase = false
+
+func _on_player_death_body_entered(body):
+	if body.name == "Player":
+		chase = false
+		get_node("AnimatedSprite2D").play("Death")	
+		await get_node("AnimatedSprite2D").animation_finished	
+		self.queue_free()
