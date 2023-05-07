49eb6a01d4d529ed6a8229a57c55276265997b27
KartikWatts <arorakartik4@gmail.com>
2023-05-08 01:03:51 +0530

    adding player idle and walk sprite animations

--- games/violest/violest/player/player.gd
+++ games/violest/violest/player/player.gd
@@ -3,4 +3,7 @@ extends CharacterBody2D
 @onready var _animation_player = $AnimationPlayer
 
 func _process(_delta):
-	_animation_player.play("idle")
+	if Input.is_action_pressed("ui_right"):
+		_animation_player.play("walk")
+	else:
+		_animation_player.play("idle")
