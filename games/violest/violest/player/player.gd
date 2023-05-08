87f4b166bde7feb5571d5a8e9dd2f795d8100f02
KartikWatts <arorakartik4@gmail.com>
2023-05-08 22:50:23 +0530

    adding player jump animation with game logic

--- games/violest/violest/player/player.gd
+++ games/violest/violest/player/player.gd
@@ -1,9 +1,37 @@
 extends CharacterBody2D
 
-@onready var _animation_player = $AnimationPlayer
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
 
-func _process(_delta):
-	if Input.is_action_pressed("ui_right"):
-		_animation_player.play("walk")
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var _animation_player = $AnimationPlayer
+@onready var _sprite_2d = $Sprite2D
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		_animation_player.play("jump_up")
+
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction == -1:
+		_sprite_2d.flip_h = true
+	elif direction == 1:
+		_sprite_2d.flip_h = false
+	
+	if direction:
+		velocity.x = direction * SPEED
+		if velocity.y == 0:
+			_animation_player.play("walk")
 	else:
-		_animation_player.play("idle")
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if velocity.y == 0:
+			_animation_player.play("idle")
+	
+	if velocity.y > 0:
+		_animation_player.play("fall_down")
+	
+	move_and_slide()
