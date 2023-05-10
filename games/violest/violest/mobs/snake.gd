d856f05457c90b32928561706532f4677e56c1ed
KartikWatts <arorakartik4@gmail.com>
2023-05-10 16:53:23 +0530

    basic snake attack logo with raycasting

--- games/violest/violest/mobs/snake.gd
+++ games/violest/violest/mobs/snake.gd
@@ -1,13 +1,34 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
+const ATTACK_RANGE = 500
 
 @onready var _animation_player = $AnimationPlayer
 @onready var _sprite_2d = $Sprite2D
 
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var direction = -1
+
 func _physics_process(delta):
-	_animation_player.play("walk")
-	var direction = 1
-	velocity.x = direction * SPEED
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	if is_on_wall() or not $FloorCheckRay.is_colliding() and is_on_floor():
+		direction *= -1
+	if $PlayerCheckRay.is_colliding():
+		velocity.x = 0
+		_animation_player.play("attack")
+	
+	else:	
+		velocity.x = direction * SPEED
+		_animation_player.play("walk")
+	
+	if direction == -1:
+		_sprite_2d.flip_h = false
+	elif direction == 1:
+		_sprite_2d.flip_h = true
+		
+	$PlayerCheckRay.target_position.x = position.x * direction + ATTACK_RANGE
+			
 	move_and_slide()
 	
--- games/violest/violest/player/player.gd
+++ games/violest/violest/player/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -500.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -14,7 +14,7 @@ func _physics_process(delta):
 	
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		print("jump up")
+#		print("jump up")
 		_animation_player.play("jump_up")
 
 	var direction = Input.get_axis("ui_left", "ui_right")
@@ -26,16 +26,16 @@ func _physics_process(delta):
 	if direction:
 		velocity.x = direction * SPEED
 		if velocity.y == 0:
-			print("walk")
+#			print("walk")
 			_animation_player.play("walk")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		if velocity.y == 0:
-			print("idle")
+#			print("idle")
 			_animation_player.play("idle")
 	
 	if velocity.y > 0:
-		print("fall down")		
+#		print("fall down")		
 		_animation_player.play("fall_down")
 	
 	move_and_slide()
