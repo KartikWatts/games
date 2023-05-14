34cb7ea478736c46884eb8a6f566958fca7a5ec9
KartikWatts <arorakartik4@gmail.com>
2023-05-14 15:48:44 +0530

    completing main menu setup

--- games/violest/violest/main.gd
+++ games/violest/violest/main.gd
@@ -1,4 +1,9 @@
 extends Node2D
 
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		_on_play_pressed()
+
+
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://world.tscn")
+	get_tree().change_scene_to_file("res://world.tscn")	
