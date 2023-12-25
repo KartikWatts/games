ab099af6e8ba1b882feba9080db01d973bff8f77
Kartik Arora <arorakartik4@gmail.com>
2023-12-25 23:07:51 +0530

    upgrades, health implementation, end screen (#11)
    * upgrade functionality with ui
    
    * project restructure and player health with UI
    
    creating 2D nodes for Entities and Foreground with YSort for better UI and clean structure in Main Scene
    reusing health_component for player with UI implementation
    
    * adding end end_screen
    
    for victory and defeat with option to restart and quit

--- 2d-survivors-course/scenes/game_object/player/player.gd
+++ 2d-survivors-course/scenes/game_object/player/player.gd
@@ -1,14 +1,22 @@
 extends CharacterBody2D
 
+@onready var _collision_area: Area2D = $CollisionArea2D
+@onready var _health_component: HealthComponent = $HealthComponent
+@onready var _damage_interval_timer: Timer = $DamageIntervalTimer
+@onready var _health_bar: ProgressBar = $HealthBar
+
 const MAX_SPEED = 125
 const ACCELERATION_SMOOTHING = 25
 
-# Called when the node enters the scene tree for the first time.
+var number_colliding_bodies = 0
+
 func _ready():
-	pass # Replace with function body.
+	_collision_area.body_entered.connect(on_body_entered)
+	_collision_area.body_exited.connect(on_body_exited)
+	_damage_interval_timer.timeout.connect(on_damage_interval_timer_timeout)
+	update_health_display()
+	_health_component.health_changed.connect(on_health_changed)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var movement_vector = get_movement_vector()
 	var direction = movement_vector.normalized()
@@ -17,9 +25,38 @@ func _process(delta):
 	velocity = velocity.lerp(target_velocity, 1 - exp(-delta * ACCELERATION_SMOOTHING))
 	move_and_slide()
 	
+
 func get_movement_vector():
 	#var movement_vector = Vector2.ZERO
-	
 	var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	return Vector2(x_movement, y_movement)
+
+
+func check_deal_damage():
+	if number_colliding_bodies == 0 || !(_damage_interval_timer.is_stopped()):
+		return
+	
+	_health_component.damage(1)
+	_damage_interval_timer.start()
+	print(_health_component.current_health)
+
+
+func update_health_display():
+	_health_bar.value = _health_component.get_health_percent()	
+
+
+func on_body_entered(other_body: Node2D):
+	number_colliding_bodies += 1
+	check_deal_damage()
+
+func on_body_exited(other_body: Node2D):
+	number_colliding_bodies -= 1
+
+
+func on_damage_interval_timer_timeout():
+	check_deal_damage()
+
+
+func on_health_changed():
+	update_health_display()
--- 2d-survivors-course/scenes/manager/upgrade_manager.gd
+++ 2d-survivors-course/scenes/manager/upgrade_manager.gd
@@ -2,6 +2,7 @@ extends Node
 
 @export var upgrade_pool: Array[AbilityUpgrade]
 @export var experience_manager: ExperienceManager
+@export var upgrade_screen_scene: PackedScene
 
 var current_upgrades = {}
 
@@ -13,12 +14,24 @@ func on_level_up(current_level: int):
 	var chosen_upgrade = upgrade_pool.pick_random() as AbilityUpgrade
 	if chosen_upgrade == null:
 		return
-		
-	var has_upgrade = current_upgrades.has(chosen_upgrade.id)
+	
+	var upgrade_screen_instance = upgrade_screen_scene.instantiate() as UpgradeScreen
+	add_child(upgrade_screen_instance)
+	upgrade_screen_instance.set_ability_upgrades([chosen_upgrade])
+	upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
+
+	
+func apply_upgrade(upgrade: AbilityUpgrade):
+	var has_upgrade = current_upgrades.has(upgrade.id)
 	if !has_upgrade:
-		current_upgrades[chosen_upgrade.id] = {
-			"resource": chosen_upgrade,
+		current_upgrades[upgrade.id] = {
+			"resource": upgrade,
 			"quantity": 1
 		}
 	else:
-		current_upgrades[chosen_upgrade.id]["quantity"] += 1
+		current_upgrades[upgrade.id]["quantity"] += 1
+	GameEvents.emit_ability_upgrade_added(upgrade, current_upgrades)
+
+
+func on_upgrade_selected(upgrade: AbilityUpgrade):
+	apply_upgrade(upgrade)
