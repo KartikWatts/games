42cc35eb9e13780a28fb646f134f99d7ea657eb7
KartikWatts <arorakartik4@gmail.com>
2024-01-02 17:56:18 +0530

    basic updates for cook game

--- react-jam-2023-winter/src/logic/logic.ts
+++ react-jam-2023-winter/src/logic/logic.ts
@@ -1,15 +1,15 @@
-import { colors, countdownDurationSeconds } from "./logicConfig.ts"
-import { updateCountdown } from "./updateCountdown.ts"
-import { updateEndOfRound } from "./updateEndOfRound.ts"
-import { updatePlaying } from "./updatePlaying/updatePlaying.ts"
-import { checkWinnersAndGameOver } from "./checkWinnersAndGameOver.ts"
-import { pickFreeColor } from "./pickFreeColor.ts"
-import { newRound } from "./newRound.ts"
-import { getRandomInitialSection } from "./getRandomInitialSection.ts"
-import { GameState } from "./types.ts"
+import { colors, countdownDurationSeconds } from "./logicConfig.ts";
+import { updateCountdown } from "./updateCountdown.ts";
+import { updateEndOfRound } from "./updateEndOfRound.ts";
+import { updatePlaying } from "./updatePlaying/updatePlaying.ts";
+import { checkWinnersAndGameOver } from "./checkWinnersAndGameOver.ts";
+import { pickFreeColor } from "./pickFreeColor.ts";
+import { newRound } from "./newRound.ts";
+import { getRandomInitialSection } from "./getRandomInitialSection.ts";
+import { GameState } from "./types.ts";
 
 Rune.initLogic({
-  minPlayers: 2,
+  minPlayers: 1,
   maxPlayers: 4,
   setup: (allPlayerIds) => {
     return {
@@ -30,29 +30,29 @@ Rune.initLogic({
             lastCollisionGridPoints: [],
           },
         }),
-        {},
+        {}
       ),
       collisionGrid: {},
       countdownTimer: countdownDurationSeconds,
       timerStartedAt: 0,
       lastRoundWinnerId: undefined,
-    }
+    };
   },
   inputDelay: 50,
   updatesPerSecond: 30,
   actions: {
     setTurning(turning, { game, playerId }) {
-      game.snakes[playerId].turning = turning
+      game.snakes[playerId].turning = turning;
     },
     setReady(_, { game }) {
-      if (game.stage !== "gettingReady") throw Rune.invalidAction()
-      newRound(game)
+      if (game.stage !== "gettingReady") throw Rune.invalidAction();
+      newRound(game);
     },
   },
   update: ({ game }) => {
-    if (game.stage === "countdown") updateCountdown(game)
-    if (game.stage === "playing") updatePlaying(game)
-    if (game.stage === "endOfRound") updateEndOfRound(game)
+    if (game.stage === "countdown") updateCountdown(game);
+    // if (game.stage === "playing") updatePlaying(game);
+    if (game.stage === "endOfRound") updateEndOfRound(game);
   },
   events: {
     playerJoined: (playerId, { game }) => {
@@ -61,25 +61,27 @@ Rune.initLogic({
         color: pickFreeColor(game.players),
         state: "pending",
         score: 0,
-      })
+      });
       game.snakes[playerId] = {
         gapCounter: 0,
         turning: "none",
         sections: [getRandomInitialSection()],
         lastCollisionGridPoints: [],
-      }
+      };
     },
     playerLeft: (playerId, { game }) => {
-      const playerIndex = game.players.findIndex((p) => p.playerId === playerId)
+      const playerIndex = game.players.findIndex(
+        (p) => p.playerId === playerId
+      );
 
-      if (!~playerIndex) throw Rune.invalidAction()
+      if (!~playerIndex) throw Rune.invalidAction();
 
-      game.players.splice(playerIndex, 1)
-      delete game.snakes[playerId]
+      game.players.splice(playerIndex, 1);
+      delete game.snakes[playerId];
 
       if (game.stage === "playing" || game.stage === "countdown") {
-        checkWinnersAndGameOver(game)
+        checkWinnersAndGameOver(game);
       }
     },
   },
-})
+});
