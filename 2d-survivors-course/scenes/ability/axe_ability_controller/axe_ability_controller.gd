33a9dca7e391d98e24d79eea99d24ac8197b245e
KartikWatts <arorakartik4@gmail.com>
2024-01-26 15:53:10 +0530

    weighted_table enhancements for upgrades
    dynamic upgrade options are implemented, along with two new upgrades for sword and axe damages.

--- 2d-survivors-course/scenes/ability/axe_ability_controller/axe_ability_controller.gd
+++ 2d-survivors-course/scenes/ability/axe_ability_controller/axe_ability_controller.gd
@@ -4,12 +4,12 @@ extends Node
 
 @onready var _timer = $Timer as Timer
 
-var damage = 10
-
+var base_damage = 10
+var additional_damage_percent = 1 
 
 func _ready():
 	_timer.timeout.connect(on_timer_timeout)
-
+	GameEvents.ability_upgrade_added.connect(on_ability_upgrade_added)
 
 func on_timer_timeout():
 	var player = get_tree().get_first_node_in_group("player") as Node2D
@@ -23,4 +23,8 @@ func on_timer_timeout():
 	var axe_instance = axe_ability_scene.instantiate() as AxeAbility
 	foreground.add_child(axe_instance)
 	axe_instance.global_position = player.global_position
-	axe_instance.hitbox_component.damage = damage
+	axe_instance.hitbox_component.damage = base_damage * additional_damage_percent
+
+func on_ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrades: Dictionary):
+	if upgrade.id == "axe_damage":
+		additional_damage_percent = 1 + (current_upgrades["axe_damage"]["quantity"] * 0.1)
