f1b742fa04bc6714cead5e24a3b615c60117c372
KartikWatts <arorakartik4@gmail.com>
2024-02-06 18:46:32 +0530

    meta progression system and upgrade cards ui
    created a Dictionary for currency and upgrades items, that gets stored as file using FileAccess read and write operations.

--- 2d-survivors-course/scenes/autoload/screen_transition.gd
+++ 2d-survivors-course/scenes/autoload/screen_transition.gd
@@ -9,15 +9,11 @@ var skip_emit = false
 
 func transition():
 	animation_player.play("default")
-	print("PLAYING FORWARD")
 	await transitioned_halfway
 	skip_emit = true
 	animation_player.play_backwards("default")
-	print("PLAYING BACKWARD")
 
 func emit_transitioned_halfway():
-	print("TRANSITION HALFWAY EMITTED")
-	print(str("SKIP EMIT: ", skip_emit))
 	if skip_emit:
 		skip_emit = false
 		return
