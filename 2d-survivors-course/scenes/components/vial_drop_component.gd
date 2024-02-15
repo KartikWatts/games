7029d4ee2696048e6e1e2499ab3bb77287d2f022
KartikWatts <arorakartik4@gmail.com>
2024-02-15 19:44:28 +0530

    creating MetaMenu UI with Cards and selectable upgrades

--- 2d-survivors-course/scenes/components/vial_drop_component.gd
+++ 2d-survivors-course/scenes/components/vial_drop_component.gd
@@ -6,9 +6,16 @@ extends Node
 
 func _ready():
 	health_component.died.connect(on_died)
+
 	
 func on_died():
-	if randf() > drop_percent:
+	var adjusted_drop_percent = drop_percent
+	var experience_gain_upgrade_count =  MetaProgression.get_upgrade_count("experience_upgrade")
+	
+	if experience_gain_upgrade_count > 0:
+		adjusted_drop_percent += .1 * experience_gain_upgrade_count
+	
+	if randf() > adjusted_drop_percent:
 		return
 		
 	if vial_scene == null:
